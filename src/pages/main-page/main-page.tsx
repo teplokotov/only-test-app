b19e50f451eadfa4970837bc2818731ecda5b9f6
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-02-06 16:40:18 +0300

    feat: add basic spinner animation

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -8,7 +8,26 @@ function MainPage() {
         <h1 className='historic-dates__heading'>Исторические даты</h1>
         <div className="historic-dates__range"></div>
         <div className="historic-dates__spinner">
-          <div className='spinner__circle'></div>
+          <div className='spinner__main-circle' style={{ "--count": 6, "--angle": "0deg" } as React.CSSProperties}>
+            <div className='spinner__shoulder' style={{ "--i": 1 } as React.CSSProperties}>
+              <p className='spinner__circle'>1</p>
+            </div>
+            <div className='spinner__shoulder' style={{ "--i": 2 } as React.CSSProperties}>
+              <p className='spinner__circle'>2</p>
+            </div>
+            <div className='spinner__shoulder' style={{ "--i": 3 } as React.CSSProperties}>
+              <p className='spinner__circle'>3</p>
+            </div>
+            <div className='spinner__shoulder' style={{ "--i": 4 } as React.CSSProperties}>
+              <p className='spinner__circle'>4</p>
+            </div>
+            <div className='spinner__shoulder' style={{ "--i": 5 } as React.CSSProperties}>
+              <p className='spinner__circle'>5</p>
+            </div>
+            <div className='spinner__shoulder' style={{ "--i": 6 } as React.CSSProperties}>
+              <p className='spinner__circle'>6</p>
+            </div>
+          </div>
         </div>
         <div className="historic-dates__navigation"></div>
         <div className="historic-dates__slider"></div>
